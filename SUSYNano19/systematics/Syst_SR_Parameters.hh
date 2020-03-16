c51e7bbbc31e8e2eb5bd34809dd9fe88f318805b
Matthew <mkilpatr@FNAL.GOV>
2020-03-15 20:56:53 -0500

    The new systematics methods works and can be merged

--- SUSYNano19/systematics/Syst_SR_Parameters.hh
+++ SUSYNano19/systematics/Syst_SR_Parameters.hh
@@ -83,7 +83,7 @@ TString phowgt() { return wgtvar(); }
 
 bool doLepSyst = false;
 // for search region = "SR", control region = "CR", for LowMET all = "LowMET", 
-TString region = "CR2016";
+TString region = "CR";
 
 // No Lepton SF
 //TString lepvetowgt() {return wgtvar();}
