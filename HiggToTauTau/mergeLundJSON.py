86f3dc98ac96658da64f4e27e6a9598a0c551cca
Matthew <mkilpatr@FNAL.GOV>
2021-06-22 14:18:21 -0500

    Total bkg added to merge json

--- HiggToTauTau/mergeLundJSON.py
+++ HiggToTauTau/mergeLundJSON.py
@@ -11,37 +11,36 @@ parser.add_argument("-d", "--dir", dest="baseDir", default='nanoAOD_diHiggs',
                          help="Base directory where files are located on EOS")
 parser.add_argument("-s", "--save", dest="saveDir", default='diHiggsJSON',
                          help="Directory where new merged files should be locally saved")
-parser.add_argument("-n", "--nEvents", dest="nEvents", default=500000,
-                         help="Directory where new merged files should be locally saved")
 args = parser.parse_args()
 
 eosDir = "/eos/uscms/store/user/mkilpatr/13TeV/" + args.baseDir + "/"
 Dist = os.listdir(eosDir)
 
-if not os.path.exists(os.path.join(args.baseDir, args.saveDir)):
-    os.makedirs(os.path.join(args.baseDir, args.saveDir))
+if not os.path.exists(args.saveDir):
+    os.makedirs(args.saveDir)
 
 def write_json(data):
     fout.write((json.dumps(data, sort_keys=False)+'\n').encode('utf-8'))
 
-def open_json(jsonfilename, nEvents):
+def writeTot_json(data):
+    ftot.write((json.dumps(data, sort_keys=False)+'\n').encode('utf-8'))
+
+def open_json(jsonfilename):
     tweets = []
     with gzip.open(jsonfilename,'r') as fin:        
         for line in fin:        
             if line == "\n": continue
             write_json(json.loads(line))
-            nEvents+=1
-            if nEvents > args.nEvents: break
-
-    return nEvents
+            if jsonfilename not in ['ggH', 'vbfH']:
+                writeTot_json(json.loads(line))
 
 print(Dist)
 
-for d in Dist:
-    if "json" in d: continue
-    subDist = os.listdir(eosDir + d)
-    for type in ['genHiggs', 'otherMatch']:
-        nEvents = 0
+for type in ['genHiggs', 'otherMatch']:
+    ftot = gzip.open(args.saveDir + "/" + type + "_bkg.json.gz", 'w')
+    for d in Dist:
+        if "json" in d: continue
+        subDist = os.listdir(eosDir + d)
         onlyFiles = []
         for sd in subDist:
             if os.path.isdir(eosDir + d + "/" + sd): continue
@@ -50,10 +49,9 @@ for d in Dist:
                     onlyFiles.append(sd)
                 elif type not in sd and 'gen' not in type:
                     onlyFiles.append(sd)
-        fout = gzip.open(os.path.join(args.baseDir, args.saveDir) + "/" + type + "_" + d + ".json.gz", 'w')
+        fout = gzip.open(args.saveDir + "/" + type + "_" + d + ".json.gz", 'w')
 
         print(eosDir + d + "/" + onlyFiles[0])
         for idx, sd in enumerate(onlyFiles):
-            nEvents = open_json(eosDir + d + "/" + sd, nEvents)
-            if nEvents > args.nEvents: break
+            open_json(eosDir + d + "/" + sd)
 
